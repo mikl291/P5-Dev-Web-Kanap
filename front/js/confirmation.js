897f88b257147d91912dbd6071fdb0ec9f2e18d8
mikl291 <larsinho91@outlook.fr>
2023-03-16 10:32:14 +0100

    Update confirmation.js

---
+++ front/js/confirmation.js
@@ -0,0 +1,26 @@
+const getOrder= () => {
+    const windowSearch = window.location.search
+    // récupère le n° de cmd
+    const urlParams = new URLSearchParams(windowSearch)
+    // retourne le n° de cmd
+    return urlParams.get("orderId")
+ }
+ 
+const displayOrder = (orderId) => {
+    // récupère le contenu du storage
+    const orderIdItem = document.getElementById("orderId")
+    orderIdItem.textContent = orderId // affiche le n° de cmd
+    // retourne le contenu du storage
+ }
+ 
+const clearCart = () => {
+    const cartCache = window.localStorage
+    // efface le contenue du storage
+    cartCache.clear()
+}
+ 
+const orderId = getOrder()
+ // obtient le n° cmd
+ displayOrder(orderId) 
+ console.log(orderId)// affiche le n° cmd
+ clearCart() // efface le panier après confiramation de cmd
